4d8670ee46a273e8dbc008dffe11d00a9cb17920
Adam Haapala <adamhaapala@gmail.com>
2019-06-25 15:51:05 -0400

    Trying a different linting hook script

--- TODO.md
+++ TODO.md
@@ -3,5 +3,7 @@
 ---
 
 - [ ] py_kanka_wrapper/kanka_wrapper.py - I don't believe I'm 100% coverage for the functionality in the wrapper to the api
-- 
-
+	- [ ] Might want to write models for each of the response types to capture all returned data 
+	- [ ] Review template manipulation I can use for both kanka and python
+	- [ ] I can still add more plural methods for creation and deletion
+	- [ ] Not sure how to use the kanka dice rolling functionality as that is what ttrpg core does and seemingly better
--- py_kanka_wrapper/kanka_wrapper.py
+++ py_kanka_wrapper/kanka_wrapper.py
@@ -4,16 +4,20 @@ import json
 
 """
    NOTES:
-   This class should have very little to do with formatting the results of the queries.  That should be left to the calling logic since display format is usually dependant on context.
-   Not all functionality of the api is addressed or leveraged in this class.  I want to establish a working minimal product before filling in the rest and as it is currently meets my needs.
-   Code and trying to utilize rolls via an api is awkward.
+   - This class should have very little to do with formatting the results
+   of the queries.  That should be left to the calling logic since display
+   format is usually dependant on context.
+
+   - Not all functionality of the api is addressed or leveraged in this
+   class.  I want to establish a working minimal product before filling in
+   the rest and as it is currently meets my needs.  Code and trying to
+   utilize rolls via an api is awkward.
 """
 
 
 class kanka_wrapper:
 
     base_url = 'https://kanka.io/api/1.0/'
-
     VALID_ENTITY_TYPES = ['characters',
                           'locations',
                           'families',
@@ -34,7 +38,9 @@ class kanka_wrapper:
                         'Accept': 'application/json'}
         self.set_campaign(campaign)
         self.current_entity = {}
-        requests_cache.install_cache(campaign + '_cache', backend='memory', expire_after=300)
+        requests_cache.install_cache(campaign + '_cache',
+                                     backend='memory',
+                                     expire_after=300)
 
     def set_campaign(self, campaign):
         self.campaign = campaign
@@ -48,11 +54,13 @@ class kanka_wrapper:
 
     def check_entity(self, entity_type, entity):
         """
-            Helper method to support referring to entities by name or by id
+            Helper method to support referring to entities
+            by name or by id
         """
         if isinstance(entity, str):
             """
-                This is an entity name so we have to look up the id.  This should now translucently cache any requests via
+                This is an entity name so we have to look up the id.
+                This should now translucently cache any requests via
                 requests_cache.
             """
             return self.get_entity_id(entity_type, entity)
@@ -69,7 +77,8 @@ class kanka_wrapper:
         """
         if isinstance(attribute, str):
             """
-                This is an attribute name so we have to look up the id.  All requests should now be cached via the requests_cache lib.
+                This is an attribute name so we have to look up the id.
+                All requests should now be cached via the requests_cache lib.
             """
             return self.get_attribute_id(entity_type, entity, attribute)
 
@@ -77,7 +86,8 @@ class kanka_wrapper:
             # This is an entity id and we can use it as is
             return attribute
         else:
-            raise(Exception('Invalid attribute identifier in check_attribute()'))
+            message = 'Invalid attribute identifier in check_attribute()'
+            raise(Exception(message))
 
     """
         Remote getters
@@ -96,7 +106,8 @@ class kanka_wrapper:
         return None
 
     """
-        Entities are any of the objects stored underneath a campaign, e.g. Characters, Locations, etc...
+        Entities are any of the objects stored underneath a campaign,
+        e.g. Characters, Locations, etc...
     """
     def get_entity(self, entity_type, entity):
         """
@@ -107,14 +118,17 @@ class kanka_wrapper:
 
         entity_id = self.check_entity(entity_type, entity)
 
-        url = self.base_url + '/campaigns/' + str(self.campaign_id) + '/' + str(entity_type) + '/' + str(entity_id)
+        url = self.base_url + '/campaigns/' + str(self.campaign_id) + '/'
+        + str(entity_type) + '/' + str(entity_id)
+
         return json.loads(requests.get(url, headers=self.headers))
 
     def get_entity_id(self, entity_type, entity_name):
         """
            entity_id is required for a lot of requests so it warrants its own
            method even if it's just wrapping get_entities and parsing for id
-           - Right now this is an exact case-sensitive match.  This can be made more fuzzy
+           - Right now this is an exact case-sensitive match.
+           This can be made more fuzzy
         """
         response = self.get_entities(entity_type)
         for entity in response['data']:
@@ -131,17 +145,18 @@ class kanka_wrapper:
         if not self.is_valid_entity_type(entity_type):
             raise(Exception('Invalid entity type in get_entities()'))
 
-        url = self.base_url + '/campaigns/' + str(self.campaign_id) + '/' + str(entity_type)
+        url = self.base_url + '/campaigns/' + str(self.campaign_id)
+        + '/' + str(entity_type)
 
         return json.loads(requests.get(url, headers=self.headers))
 
     """
         Attributes are custom fields tacked onto an entity either via template
-        or manual addition.  For my puproses these are game mechanics and things
-        off of a character sheet.
+        or manual addition.  For my puproses these are game mechanics and
+        things off of a character sheet.
         Notes:
-        - I may just use the entity get call to retrieve attributes since they should be a subset of
-        the entity.
+        - I may just use the entity get call to retrieve attributes since they
+        should be a subset of the entity.
     """
     def get_attribute(self, entity_type, entity, attribute):
         """
@@ -149,19 +164,24 @@ class kanka_wrapper:
         """
         entity_id = self.check_entity(entity_type, entity)
         attribute_id = self.check_attribute(entity_type, entity_id, attribute)
-        url = self.base_url + '/' + str(self.campaign_id) + '/entities/' + str(entity_id) + '/attributes/' + str(attribute_id)
+        url = self.base_url + '/' + str(self.campaign_id) + '/entities/'
+        + str(entity_id) + '/attributes/' + str(attribute_id)
+
         return json.loads(requests.get(url, headers=self.headers))
 
     def get_attributes(self, entity_id):
         """
             Get all attributes for an entity
         """
-        url = self.base_url + '/campaign/' + str(self.campaign_id) + '/entities/' + str(entity_id) + '/attributes'
+        url = self.base_url + '/campaign/' + str(self.campaign_id)
+        + '/entities/' + str(entity_id) + '/attributes'
+
         return json.loads(requests.get(url, headers=self.headers))
 
     def get_attribute_id(self, entity_type, entity, attribute_name):
         """
-            Like for entities, you need a numeric id to look up specific entries
+            Like for entities, you need a numeric id to look up specific
+            entries
         """
         entity_id = self.check_entity(entity_type, entity)
         attributes = self.get_attributes(entity_id)
@@ -177,23 +197,27 @@ class kanka_wrapper:
     """
     def update_attribute(self, entity_type, entity, attribute_name, value):
         """
-            Just going with bare minimum for post body.  There's important other
-            possible fields that should be supported.
+            Just going with bare minimum for post body.  There's important
+            other possible fields that should be supported.
         """
         entity_id = self.check_entity(entity_type, entity)
-        attribute_id = self.check_attribute(entity_type, entity_id, attribute_name)
+        attribute_id = self.check_attribute(entity_type,
+                                            entity_id,
+                                            attribute_name)
 
         body = {'name': attribute_name,       # attribute is required
                 'value': value,
                 'entity_id': entity_id}            # entity_id is required
 
-        url = self.base_url + '/campaign/' + str(self.campaign_id) + '/entities/' + str(entity_id) + '/attributes/' + str(attribute_id)
+        url = self.base_url + '/campaign/' + str(self.campaign_id)
+        + '/entities/' + str(entity_id) + '/attributes/' + str(attribute_id)
 
         return json.loads(requests.put(url, data=body, headers=self.headers))
 
     def update_attributes(self, entity_type, entity, attributes_and_values):
         """
-            Plural attribute setter.  Takes a dictionary e.g. {'attribute name': 'attribute value'}
+            Plural attribute setter.  Takes a dictionary
+            e.g. {'attribute name': 'attribute value'}
         """
         for key, value in attributes_and_values.items():
             yield self.update_attribute(entity_type, entity, key, value)
@@ -203,12 +227,15 @@ class kanka_wrapper:
             Going to leave populating the post body for the calling logic
         """
         entity_id = self.check_entity(entity_type, entity)
-        url = self.base_url + '/campaign/' + str(self.campaign_id) + '/' + str(entity_type) + '/' + str(entity_id)
+        url = self.base_url + '/campaign/' + str(self.campaign_id)
+        + '/' + str(entity_type) + '/' + str(entity_id)
+
         return json.loads(requests.put(url, headers=self.headers, data=body))
 
     def update_entities(self, entity_type, entities):
         """
-            Plural entity setter.  Takes a dictionary e.g. {'entity name': {<entity body here>}}
+            Plural entity setter.  Takes a dictionary
+            e.g. {'entity name': {<entity body here>}}
         """
         for entity_name, entity_body in entities.items():
             yield self.update_entity(entity_type, entity_name, entity_body)
@@ -222,11 +249,15 @@ class kanka_wrapper:
                 'value': value,
                 'entity_id': entity_id}
 
-        url = self.base_url + '/campaign/' + str(self.campaign_id) + '/entities/' + str(entity_id) + '/attributes/'
+        url = self.base_url + '/campaign/' + str(self.campaign_id)
+        + '/entities/' + str(entity_id) + '/attributes/'
+
         return json.loads(requests.post(url, data=body, headers=self.headers))
 
     def create_entity(self, entity_type, entity_name, body):
-        url = self.base_url + '/campaign/' + str(self.campaign_id) + '/' + str(entity_type)
+        url = self.base_url + '/campaign/' + str(self.campaign_id)
+        + '/' + str(entity_type)
+
         return json.loads(requests.post(url, data=body, heades=self.headers))
 
     """
@@ -235,12 +266,17 @@ class kanka_wrapper:
     def delete_attribute(self, entity_type, entity, attribute):
         entity_id = self.check_entity(entity_type, entity)
         attribute_id = self.check_attribute(entity_type, entity_id, attribute)
-        url = self.base_url + '/campaign/' + str(self.campaign_id) + '/entities/' + str(entity_id) + '/attributes/' + str(attribute_id)
+        url = self.base_url + '/campaign/'
+        + str(self.campaign_id) + '/entities/' + str(entity_id)
+        + '/attributes/' + str(attribute_id)
+
         return json.loads(requests.delete(url, headers=self.headers))
 
     def delete_entity(self, entity_type, entity):
         entity_id = self.check_entity(entity_type, entity)
-        url = self.base_url + '/campaign/' + str(self.campaign_id) + '/' + str(entity_type) + '/' + str(entity_id)
+        url = self.base_url + '/campaign/' + str(self.campaign_id) + '/'
+        + str(entity_type) + '/' + str(entity_id)
+
         return json.loads(requests.delete(url, headers=self.headers))
 
     """
